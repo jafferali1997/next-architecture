f1a5d7a99e33d3989a9b1e68341067a5f258cd6a
jaffer ali <jafferali1997@gmail.com>
2023-05-24 18:22:07 +0500

    changed a comment

--- src/auth/private.component.jsx
+++ src/auth/private.component.jsx
@@ -5,7 +5,7 @@ import { isAccessTokenVerifed } from '@/common/utils/access-token.util';
 /**
  * Return the component if access token is verified and return to home page if its not
  * @param {component} props take a component
- * @returns component | home page
+ * @returns component | redirect to home page
  */
 export default function Private({ component }) {
   const router = useRouter();
