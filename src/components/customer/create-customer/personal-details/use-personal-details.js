ac61378b965f44a511d4af2a7d524e35828811c4
Areeb Mughal <areebarshad.m@gmail.com>
2023-06-06 20:27:21 +0500

    UPDATE: add and fectch price group and discount group API integrated, add personal detail API integrated

--- src/components/customer/create-customer/personal-details/use-personal-details.js
+++ src/components/customer/create-customer/personal-details/use-personal-details.js
@@ -1,56 +1,64 @@
-import { yupResolver } from '@hookform/resolvers/yup';
 import axios from 'axios';
 import { City, Country } from 'country-state-city';
 import { useRouter } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { createCustomerPersonalDetail } from '@/provider/features/customer/customer.slice';
+import {
+  createDiscountGroup,
+  getAllDiscountGroup
+} from '@/provider/features/discount-group/discount-group.slice';
+import { createPriceGroup } from '@/provider/features/price-group/price-group.slice';
+import { getAllPriceGroup } from '@/provider/features/price-group/price-group.slice';
 // import CustomAlert from '@/common/components/custom-alert';
 
 const validationSchema = yup.object({
   // Define your validation rules here.
-  // gender: yup.string().required('Gender is required'),
-  // firstName: yup
-  //   .string()
-  //   .max(50, 'first name must be at most 50 characters long')
-  //   .min(1, 'first name must be minimum 1 characters')
-  //   .required('First name is required'),
-  // lastName: yup
-  //   .string()
-  //   .max(50, 'last name must be at most 50 characters long')
-  //   .min(1, 'last name must be minimum 1 characters')
-  //   .required('Last name is required'),
-  // designation: yup
-  //   .string()
-  //   .max(100, 'designation must be at most 100 characters long')
-  //   .min(1, 'designation must be minimum 1 characters')
-  //   .matches(/^[^.]*$/, {
-  //     message: 'Invalid designation'
-  //   })
-  //   .matches(/^[^!@#$%^&*+=<>:;|~(){}[\]]*$/, {
-  //     message: 'Invalid designation'
-  //   })
-  //   .required('designation is required'),
-  // postal: yup
-  //   .number()
-  //   .max(9999999999, 'postal code must be at most 10 characters long')
-  //   .min(1, 'postal code must be minimum 1 characters')
-  //   // .matches(/^[^.]*$/, {
-  //   //   message: 'No period'
-  //   // })
-  //   // .matches(/^[^.]*$/, {
-  //   //   message: 'Invalid postal'
-  //   // })
-  //   // .matches(/^[^!@#$%^&*+=<>:;|~(){}[\s\]]*$/, {
-  //   //   message: 'Invalid postal'
-  //   // })
-  //   .required('postal code is required'),
-  // address: yup
-  //   .string()
-  //   .max(95, 'address must be at most 95 characters long')
-  //   .min(1, 'address must be minimum 1 characters')
-  //   .required('address is required'),
-  // country: yup.string().required('country is required')
+  gender: yup.string().required('Gender is required'),
+  firstName: yup
+    .string()
+    .max(50, 'first name must be at most 50 characters long')
+    .min(1, 'first name must be minimum 1 characters')
+    .required('First name is required'),
+  lastName: yup
+    .string()
+    .max(50, 'last name must be at most 50 characters long')
+    .min(1, 'last name must be minimum 1 characters')
+    .required('Last name is required'),
+  designation: yup
+    .string()
+    .max(100, 'designation must be at most 100 characters long')
+    .min(1, 'designation must be minimum 1 characters')
+    .matches(/^[^.]*$/, {
+      message: 'Invalid designation'
+    })
+    .matches(/^[^!@#$%^&*+=<>:;|~(){}[\]]*$/, {
+      message: 'Invalid designation'
+    })
+    .required('designation is required'),
+  postal: yup
+    .number()
+    .max(9999999999, 'postal code must be at most 10 characters long')
+    .min(1, 'postal code must be minimum 1 characters')
+    // .matches(/^[^.]*$/, {
+    //   message: 'No period'
+    // })
+    // .matches(/^[^.]*$/, {
+    //   message: 'Invalid postal'
+    // })
+    // .matches(/^[^!@#$%^&*+=<>:;|~(){}[\s\]]*$/, {
+    //   message: 'Invalid postal'
+    // })
+    .required('postal code is required'),
+  address: yup
+    .string()
+    .max(95, 'address must be at most 95 characters long')
+    .min(1, 'address must be minimum 1 characters')
+    .required('address is required'),
+  country: yup.string().required('country is required')
 });
 
 export default function usePersonalDetails({ handleTabClick, handleTabCompleted }) {
@@ -74,6 +82,8 @@ export default function usePersonalDetails({ handleTabClick, handleTabCompleted 
   const [discountOptions, setDiscountOptions] = useState([]);
   const [isSubmit, setIsSubmit] = useState(false);
   const countries = Country.getAllCountries();
+
+  const dispatch = useDispatch();
 
   const fetchData = useCallback(
     async (id) => {
@@ -102,40 +112,47 @@ export default function usePersonalDetails({ handleTabClick, handleTabCompleted 
   );
 
   const fetchPriceGroup = async () => {
-    const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/price-group/get-all`
-    );
-    setPriceGroup(
-      res.data.result.data.priceGroup.map((item) => {
-        return { value: item._id, label: item.name };
-      })
-    );
+    const res = dispatch(getAllPriceGroup());
+    console.log(res, 'Price Groups');
+
+    // const res = await axios.get(
+    //   `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/price-group/get-all`
+    // );
+    // setPriceGroup(
+    //   res.data.result.data.priceGroup.map((item) => {
+    //     return { value: item._id, label: item.name };
+    //   })
+    // );
   };
 
   const fetchDiscountGroup = async () => {
-    const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/discount-group/get-all`
-    );
-    setDiscountGroup(
-      res.data.result.data.discountGroup.map((item) => {
-        return { value: item._id, label: item.name };
-      })
-    );
+    const res = dispatch(getAllDiscountGroup());
+    console.log(res, 'Discount Groups');
+    // const res = await axios.get(
+    //   `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/discount-group/get-all`
+    // );
+    // setDiscountGroup(
+    //   res.data.result.data.discountGroup.map((item) => {
+    //     return { value: item._id, label: item.name };
+    //   })
+    // );
   };
 
   const addPriceGroup = async (data) => {
-    const res = await axios.post(
-      `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/price-group/create`,
-      data
-    );
+    dispatch(createPriceGroup({ payload: data }));
+    // const res = await axios.post(
+    //   `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/price-group/create`,
+    //   data
+    // );
     fetchPriceGroup();
   };
 
   const addDiscountGroup = async (data) => {
-    const res = await axios.post(
-      `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/discount-group/create`,
-      data
-    );
+    dispatch(createDiscountGroup({ payload: data }));
+    // const res = await axios.post(
+    //   `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/discount-group/create`,
+    //   data
+    // );
     fetchDiscountGroup();
   };
 
@@ -167,8 +184,27 @@ export default function usePersonalDetails({ handleTabClick, handleTabCompleted 
 
   const onSubmit = async (value) => {
     console.log(value);
-    handleTabClick('company_details');
-    handleTabCompleted('customer_details');
+    const data = {
+      ...value,
+      ...(router.query.id && { customerId: router.query.id }),
+      priceGroups: [
+        ...priceOptions.map((item) => {
+          return item.value;
+        })
+      ],
+      discountGroups: [
+        ...discountOptions.map((item) => {
+          return item.value;
+        })
+      ]
+    };
+    const res = dispatch(createCustomerPersonalDetail({ payload: data }));
+    if (res) {
+      router.query.id = res.data.result.data._id;
+      router.push(router);
+      handleTabClick('company_details');
+      handleTabCompleted('customer_details');
+    }
     // try {
     //   const res = await axios.post(
     //     `${process.env.NEXT_PUBLIC_MAIN_URL}/administration/customer/personal-detail`,
