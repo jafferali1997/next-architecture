7083a56b8adfb15fe047b2b956cfe0d43a0d08f8
AreebMughal <areebarshad.m@gmail.com>
2023-06-14 18:46:01 +0500

    UPDATE: general changes

--- src/common/components/custom-select/custom-select.component.jsx
+++ src/common/components/custom-select/custom-select.component.jsx
@@ -42,7 +42,7 @@ export default function CustomSelect({
                 {...field}
                 className="tw-h-[40px] tw-w-full !tw-py-0 tw-px-[18px] tw-font-dm tw-text-text-dark-gray placeholder:tw-text-text-ultra-light-gray"
               >
-                {options.map((item) => (
+                {options?.map((item) => (
                   <MenuItem key={item.value} value={item.value}>
                     {item.label}
                   </MenuItem>
@@ -52,19 +52,19 @@ export default function CustomSelect({
           />
         )}
 
-        {/* {!control && (
+        {!control && (
           <Select
             name={name}
             defaultValue={defaultValue}
             className="default-input input-field"
           >
-            {options.map((item) => (
+            {options?.map((item) => (
               <MenuItem key={item.value} value={item.value}>
                 {item.label}
               </MenuItem>
             ))}
           </Select>
-        )} */}
+        )}
 
         {errors && errors[name] && (
           <FieldError className="tw-mt-1" error={errors[name].message} />
